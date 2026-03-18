acf6f47ff0be733c13888e560804bb0058f185fc
Miikki10 <bruno.milicevic12@gmail.com>
2026-03-18 12:01:57 +0100

    Create initial PHP document with HTML structure

--- Vjezba1/vjezba1.php
+++ Vjezba1/vjezba1.php
@@ -1,1 +1,28 @@
+<?php
 
+$naslov = "Moj prvi PHP dokument";
+$autor = "Bruno Miličević"; 
+?>
+<!DOCTYPE html>
+<html lang="hr">
+<head>
+    <meta charset="UTF-8">
+    <title><?php echo $naslov; ?></title>
+</head>
+<body>
+
+<?php
+
+echo "<h1>$naslov</h1>";
+
+
+echo "<p>Ovu stranicu izradio/la je <strong>$autor</strong>.</p>";
+
+
+echo '<a href="https://www.google.com" target="_blank">Posjeti Google</a>';
+?>
+
+<!-- Naziv datoteke: vjezba1.php -->
+
+</body>
+</html>
